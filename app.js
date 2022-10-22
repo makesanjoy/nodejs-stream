cabc76976e4e3bde320efaf8bb92ee93996484e5
sanjay singha <make.sanjoy@gmail.com>
2022-10-22 23:51:38 +0530

    Added new method for reading data

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ server.on("request",(req,res)=>{
     })
 })
 
+//new method of reading data
+server.on("request",(req,res)=>{
+const rstream = fs.createReadStream('index.txt');
+ rstream.on("data",(chunkdata)=>{
+    res.write(chunkdata);
+ })
+ rstream.on("end",()=>{
+    res.end();
+ });
+ rstream.on("error",()=>{
+    console.log(err);
+    res.end("File not found");
+ })
+});
+
 server.listen(3000,()=>{
     console.log("Server Listening on port 3000");
 })
